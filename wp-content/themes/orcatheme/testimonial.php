2c926104bfcb7edd3c98a20bdb3f38473587d08a
rebeccakorwinlind <reblin01@easv365.dk>
2026-09-01 12:03:16 +0200

    do styling for testimonials

--- wp-content/themes/orcatheme/testimonial.php
+++ wp-content/themes/orcatheme/testimonial.php
@@ -7,24 +7,14 @@ get_header();
 
 <main class="testimonial-page">
     <?php
-
-/* Stores the status message that will be shown after submission.*/
-
     $message = '';
-
- /* If the form is submitted, this will validate the request and save the review as a pending testimonial.*/
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['testimonial_submit'])) {
         if (!isset($_POST['orca_testimonial_nonce']) || !wp_verify_nonce($_POST['orca_testimonial_nonce'], 'orca_submit_testimonial')) {
             $message = 'Security check failed.';
         } else {
-
-/* Clean the input before saving it.*/
-       
             $name = sanitize_text_field(wp_unslash($_POST['testimonial_name']));
             $review = sanitize_textarea_field(wp_unslash($_POST['testimonial_review']));
-
-/* Only create the testimonial if both fields are filled. */
 
             if ($name && $review) {
                 $post_id = wp_insert_post(array(
@@ -33,9 +23,6 @@ get_header();
                     'post_type'    => 'testimonial',
                     'post_status'  => 'pending',
                 ));
-
-/* Shows a success or error message based on whether the post was created.*/
-
 
                 if ($post_id) {
                     $message = 'Thank you! Your review has been submitted for approval.';
